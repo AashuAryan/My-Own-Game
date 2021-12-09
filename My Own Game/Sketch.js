983ea2535188fb320900c948cc8aad02efe66047
AashuAryan <82717211+AashuAryan@users.noreply.github.com>
2021-12-09 20:51:30 +0530

    Add files via upload

---
+++ My Own Game/Sketch.js
@@ -0,0 +1,9 @@
+function preload(){
+    Parents = loadImage("Animations/FightingParents.jpeg")
+    Child = loadImage("Animations/FrighttenChild.jpg")
+    House = loadImage("Animations/HouseRoom.jpg")
+    Office = loadImage("Animations/OfficeWorker.jpg")
+    Stars = loadImage("Animations/WatchingStars.webp")
+    
+}
+
